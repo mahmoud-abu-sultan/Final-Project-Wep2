b51de405b32b2c027602e43f97b85f4e1ed198e6
Ahmad Nahal <ahmadGhssanNahal@gmail.com>
2022-05-15 13:48:15 +0300

    Add Controller

---
+++ Dashborde/controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'Controller.php';
+class UserController extends Controller {
+
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+
+    public function store()
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function update($id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+}
